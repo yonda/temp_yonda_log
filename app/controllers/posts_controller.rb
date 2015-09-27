1f192488dc879107c3550ff5de61285d8260301e
yonda <honda@33i.co.jp>
2015-09-27 13:28:29 +0900

    投稿や一覧を一通りできるようにした

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,24 @@
 class PostsController < ApplicationController
   def new
   end
+
+  def create
+    @post = Post.create(post_params)
+
+    redirect_to @post
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def index
+    @posts = Post.all
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
 end
